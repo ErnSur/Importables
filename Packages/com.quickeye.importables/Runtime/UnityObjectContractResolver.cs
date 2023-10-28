9d05a7b2c574bbb63cd090a1e544b680d9b4cf02
Ernest Suryś <ernest.surys@gmail.com>
2023-10-28 19:09:05 +0200

    cr

--- Packages/com.quickeye.importables/Runtime/UnityObjectContractResolver.cs
+++ Packages/com.quickeye.importables/Runtime/UnityObjectContractResolver.cs
@@ -1,29 +1,20 @@
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Object = UnityEngine.Object;
 
 namespace QuickEye.ImportableAssets
 {
-    public abstract class UnityObjectContractResolver : DefaultContractResolver
+    public class UnityObjectContractResolver : DefaultContractResolver
     {
         private readonly HashSet<string> _ignoreProps = new HashSet<string>
         {
             "name", "hideFlags"
         };
 
-        protected abstract JsonConverter<Object> UnityObjectConverter { get; }
-
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
             var property = base.CreateProperty(member, memberSerialization);
-            
-            if (IsUnityEngineObject(property.PropertyType))
-            {
-                property.Converter = UnityObjectConverter;
-            }
 
             if (_ignoreProps.Contains(property.PropertyName))
             {
@@ -31,13 +22,6 @@ namespace QuickEye.ImportableAssets
             }
 
             return property;
-        }
-
-        private static bool IsUnityEngineObject(Type type)
-        {
-            if (type == null)
-                return false;
-            return typeof(UnityEngine.Object).IsAssignableFrom(type);
         }
     }
 }
---
+++ Packages/com.quickeye.importables/Tests/Editor/JsonConverterTests.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace QuickEye.ImportableAssets.Editor.Tests
+{
+    public class JsonConverterTests
+    {
+        private static IEnumerable<JsonConverter> UnityObjectConverters
+        {
+            get
+            {
+                yield return new RuntimeUnityObjectConverter();
+                yield return new EditorUnityObjectConverter();
+            }
+        }
+
+        private ScriptableObject _asset;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _asset = Resources.Load<ScriptableObject>("Dog");
+        }
+        
+        [TestCaseSource(nameof(UnityObjectConverters))]
+        public void Should_DeserializeUnityObjects(JsonConverter unityObjectJsonConverter)
+        {
+            var expected = new TestObj
+            {
+                Field = _asset,
+                Field2 = _asset,
+                Nested = new TestObj()
+                {
+                    Field = _asset
+                },
+                Array = new Object[]
+                {
+                    _asset,
+                    _asset
+                },
+                List = new List<Object>
+                {
+                    _asset,
+                    _asset
+                },
+                Dic = new Dictionary<string, Object>()
+                {
+                    { "1", _asset },
+                    { "2", _asset },
+                }
+            };
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(unityObjectJsonConverter);
+            settings.Formatting = Formatting.Indented;
+
+            var json = JsonConvert.SerializeObject(expected, settings);
+            var newObj = JsonConvert.DeserializeObject<TestObj>(json, settings);
+
+            Assert.AreEqual(expected.Field, newObj.Field, nameof(TestObj.Field));
+            Assert.AreEqual(expected.Field2, newObj.Field2, nameof(TestObj.Field2));
+            Assert.AreEqual(expected.Nested.Field, newObj.Nested.Field, nameof(TestObj.Nested));
+            CollectionAssert.AreEquivalent(expected.Array, newObj.Array, nameof(TestObj.Array));
+            CollectionAssert.AreEquivalent(expected.List, newObj.List, nameof(TestObj.List));
+            CollectionAssert.AreEquivalent(expected.Dic, newObj.Dic, nameof(TestObj.Dic));
+        }
+    }
+}
